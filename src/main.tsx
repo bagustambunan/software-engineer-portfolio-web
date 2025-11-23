ea44d5f6adf611e1ff2b31c7966bb94d9862ebc7
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2025-11-23 19:39:05 +0700

    update loading fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,41 @@
-import { lazy, StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './index.css'
-import routes from './constants/route'
-import Layout from './app-layout/Layout'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import routes from "./constants/route";
+import Layout from "./app-layout/Layout";
+import { profile } from "./constants/profile";
 
-const HomePage = lazy(() => import('./pages/Home'))
-const ExperiencesPage = lazy(() => import('./pages/Experiences'))
-const ProjectPage = lazy(() => import('./pages/Projects'))
-const AchievementsPage = lazy(() => import('./pages/Achievements'))
-const ContactPage = lazy(() => import('./pages/Contact'))
-const ResumePage = lazy(() => import('./pages/Resume'))
+const HomePage = lazy(() => import("./pages/Home"));
+const ExperiencesPage = lazy(() => import("./pages/Experiences"));
+const ProjectPage = lazy(() => import("./pages/Projects"));
+const AchievementsPage = lazy(() => import("./pages/Achievements"));
+const ContactPage = lazy(() => import("./pages/Contact"));
+const ResumePage = lazy(() => import("./pages/Resume"));
 
 const LoadingFallback = () => (
-  <div style={{
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%',
-    fontSize: '18px'
-  }}>
-    Loading...
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      width: "100%",
+    }}
+  >
+    <img
+      src={profile.photo}
+      alt={profile.name}
+      title={profile.name}
+      style={{
+        width: "80px",
+        height: "80px",
+      }}
+    />
   </div>
-)
+);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Suspense fallback={<LoadingFallback />}>
@@ -41,5 +51,5 @@ createRoot(document.getElementById('root')!).render(
         </Routes>
       </Suspense>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
